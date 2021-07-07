039eadd44eeebfd43bbdd8264ab028097bcfda8a
Jay <jaystance25@gmail.com>
2021-07-07 13:45:27 -0700

    'Dashboard'

---
+++ dash.js
@@ -0,0 +1,31 @@
+let userName = "Promise"
+
+let elements = [1, 2, 3, 4, 5, 6, 7]
+
+for (let i = 0; i < 10; i++) {
+    if (userName == "Promise") {
+        for (let i in elements) {
+            console.log(i)
+        }
+    }
+}
+
+let numbers = []
+
+for (let i = 0; i < 10; i++) {
+    if (i % 2 == 0 && i % 12 == 0) {
+        numbers.push(i)
+    }
+}
+
+let fruits = ['banana', 'apple', 'pineapple']
+
+for (let fruit of fruits) {
+    console.log(fruit)
+}
+
+
+// This is  a comment in the computer program
+
+
+// I am going to hack your computer now you mother fucker.
